1094a39dbf41effdde3bee6a4a6fcead699b3ce6
HarshKakran <harshkakran05@gmail.com>
2020-10-10 21:15:08 +0530

    Changed the tree, initialized Django templating.

--- styjm/home/urls.py
+++ styjm/home/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from home import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('contact-us/', TemplateView.as_view(template_name='home/contact.html')),
+    path('about-us/', TemplateView.as_view(template_name='home/about.html')),
     ]
